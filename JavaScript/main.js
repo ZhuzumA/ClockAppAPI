900c1fe2c25a18cdae2e10117c567d1456ed0564
ZhuzumA <Zhuzum@gmail.com>
2022-10-28 01:11:04 +0200

    Set greetings according daytime

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -28,7 +28,7 @@ function getQuote() {
 .catch(error => console.log('Error'))
 }
 
-//gettin user's time and date data
+//gettin user's time and location data 
 function getTime() {
 	fetch("http://worldtimeapi.org/api/ip")
 .then(res => {
@@ -41,12 +41,61 @@ function getTime() {
 	document.getElementById('day-year').innerHTML = objTime.day_of_year;
 	document.getElementById('day-week').innerHTML = objTime.day_of_week;
 	document.getElementById('week-number').innerHTML = objTime.week_number;
+	document.getElementById('current-zone').innerHTML = objTime.timezone;
 	//getting current time 
 	const timeStr = objTime.datetime;
 	let currentTime = timeStr.substr(11,5);
 	document.getElementById('clock').innerHTML = currentTime;
+
+	//setting greetings according to the time
+	let greetingsTime = timeStr.substr(11,2);
+	switch (parseInt(greetingsTime)) {
+		case (0||1||2||3||4||5||6||7||8||9||10||11):
+		  console.log('Good mornig');
+		  document.getElementById('greetings').innerHTML = "Good morning";
+		  break;
+		case 12:
+		  console.log("It's lunch time");
+		  document.getElementById('greetings').innerHTML = "It's lunch time";
+		  break;
+		case (13||14||15||16):
+		  console.log('Good afternoon');
+		  document.getElementById('greetings').innerHTML = "Good afternoon";
+		  break;
+		case (17||18||19||20):
+		  console.log('Good evening');
+		  document.getElementById('greetings').innerHTML = "Good evening";
+		  break;
+		case (21||22||23):
+		  console.log('Good night');
+		  document.getElementById('greetings').innerHTML = "Good night";
+		  break;
+		default:
+			console.log('Er')
+	}
 })
 .catch(error => console.log('Error'))
 }
 
 getTime();
+
+/*getting user's country data
+function getCountry() {
+		fetch("ipinfo.io/77.4.46.111?token=5a5e0b15098642")
+	.then(res => {
+		return res.json()
+	})
+	.then(function(data) {
+		const objCountry = data;
+		console.log(objCountry);
+	
+	//	document.getElementById('quote-content').innerHTML = quoteText;
+	
+	})
+	.catch(error => console.log('Error'))
+	}
+
+	getCountry();
+	*/
+
+
